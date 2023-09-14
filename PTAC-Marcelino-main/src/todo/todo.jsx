15246804e597527e39046f5a4e0a7e9932b1a49a
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2023-09-14 19:49:33 -0400

    concertando erros

--- PTAC-Marcelino-main/src/todo/todo.jsx
+++ PTAC-Marcelino-main/src/todo/todo.jsx
@@ -6,71 +6,67 @@ import './estilos.css'
 
 export default function Todo() {
   const [lista, setLista] = useState([]);
-  const [id, setid] = useState(1);
-  const [Atividade, setAtividade] = useState("");
+  const [id, setId] = useState(1);
   const [Nome, setNome] = useState("");
+  const [Cpf, setCpf] = useState("");
   const [Idade, setIdade] = useState("");
 
   function salvar(e) {
     e.preventDefault();
-    setLista([...lista, { Atividade: Atividade, id: id, Nome: Nome, Idade: Idade 
-    }]);
-    setid(id + 1);
-    setAtividade("");
+    setLista([...lista, { id: id, Nome: Nome, Cpf: Cpf, Idade: Idade }]);
+    setId(id + 1);
+    setNome("");
+    setCpf("");
+    setIdade("");
   }
 
   const remover = (id) => {
-    const listaBest = [];
-    lista.map((lista) => {
-      if (lista.id !== id) {
-        listaBest.push(lista);
-      }
-  });
-  setLista(listaBest);
-
+    const listaFiltrada = lista.filter((item) => item.id !== id);
+    setLista(listaFiltrada);
   }
 
-return (
-  <div className="container">
-    <h1>TODO</h1>
-    <Link to="/home" className="button-link">App</Link>
-    <h1>Seus dados</h1>
-    <form onSubmit={salvar}>
-      <div className="input-container">
-        <input
-          className="pink-border-input"
-          onChange={(e) => setAtividade(e.target.value)}
-          type="text"
-          value={Nome}
+  return (
+    <div className="container">
+      <h1>TODO</h1>
+      <Link to="/home" className="button-link">App</Link>
+      <h1>Seus dados</h1>
+      <form onSubmit={salvar}>
+        <div className="input-container">
+          <input
+            className="pink-border-input"
+            onChange={(e) => setNome(e.target.value)}
+            type="text"
+            value={Nome}
           />
-       
-       <input
-          className="pink-border-input"
-          onChange={(e) => setAtividade(e.target.value)}
-          type="text"
-          value={Cpf}/>
-
-        <input
-          className="pink-border-input"
-          onChange={(e) => setAtividade(e.target.value)}
-          type="text"
-          value={Idade}/>
-          
+          <input
+            className="pink-border-input"
+            onChange={(e) => setCpf(e.target.value)}
+            type="text"
+            value={Cpf}
+          />
+          <input
+            className="pink-border-input"
+            onChange={(e) => setIdade(e.target.value)}
+            type="text"
+            value={Idade}
+          />
           <button className="pink-border-button">add</button>
-      </div>
-    </form>
+        </div>
+      </form>
 
-    <div> {/* Deixe esta div sem classes */}
-      <div className="values-area"> {/* Aplicar a classe "values-area" aqui */}
-        {lista.map((atividade) => (
-          <div key={atividade.id}>
-            <p>{atividade.id}</p>
-            <p>{atividade.Atividade}</p>
-          </div>
-        ))}
+      <div>
+        <div className="values-area">
+          {lista.map((item) => (
+            <div key={item.id}>
+              <p>ID: {item.id}</p>
+              <p>Nome: {item.Nome}</p>
+              <p>CPF: {item.Cpf}</p>
+              <p>Idade: {item.Idade}</p>
+              <button onClick={() => remover(item.id)}>Remover</button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
 }
