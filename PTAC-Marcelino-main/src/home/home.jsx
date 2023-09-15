d2d43ff24cb6499fb6d5659dd5067eced3c097a0
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2023-09-14 20:15:19 -0400

    tetando deixae

--- PTAC-Marcelino-main/src/home/home.jsx
+++ PTAC-Marcelino-main/src/home/home.jsx
@@ -4,10 +4,10 @@
 // ...
 
 
-
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import './home.css';
+import './sua-imagem.png'
 
 export default function Home() {
     const [name, setName] = useState("");
@@ -15,10 +15,9 @@ export default function Home() {
     return (
         <div className="container">
             <h1 className="h1-pink">Home</h1>
-            <Link to="/todo" className="button-link">App</Link> {/* Aplicar a classe "button-link" aqui */}
+            <Link to="/todo" className="button-link">App</Link>
+            <img src="/sua-imagem.png"  /> 
             <p>{name}</p>
-           
         </div>
-        
     ); 
 }
