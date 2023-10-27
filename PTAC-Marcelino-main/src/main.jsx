5282a116a8ab23476da25995c7f65cb04b2d7412
belaSimoes <isabelasimoes205@gmail.com>
2023-10-27 17:36:22 -0400

    detalhe

--- PTAC-Marcelino-main/src/main.jsx
+++ PTAC-Marcelino-main/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import Home from './home/home.jsx'
 import Todo from './todo/todo.jsx'
   import {Routes,Route,BrowserRouter} from "react-router-dom"
+  import Detalhe from './detalhe/Detalhe.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -10,8 +11,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Routes>
       <Route path='/' element={<Home />}></Route>
       <Route path='/todo' element={<Todo />}></Route>
+      <Route path='/detalhe/:id' element={<Detalhe />}></Route>
     </Routes>
     </BrowserRouter>
     
   </React.StrictMode>,
 )
+
